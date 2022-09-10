61e0953bd5090bb8a87abed3d2ddfb2819423546
WeslleyIvis <weslley_reive@hotmail.com>
2022-09-10 00:33:17 -0300

    Testando css em e automação de elementos em react

--- src/App.js
+++ src/App.js
@@ -1,87 +1,66 @@
 import React from 'react';
-
-const formFields = [
-  {
-    id: 'nome',
-    label: 'Nome',
-    type: 'text',
-  },
-  {
-    id: 'email',
-    label: 'Email',
-    type: 'email',
-  },
-  {
-    id: 'senha',
-    label: 'Senha',
-    type: 'password',
-  },
-  {
-    id: 'cep',
-    label: 'Cep',
-    type: 'text',
-  },
-  {
-    id: 'rua',
-    label: 'Rua',
-    type: 'text',
-  },
-  {
-    id: 'bairro',
-    label: 'Bairro',
-    type: 'text',
-  },
-  {
-    id: 'cidade',
-    label: 'Cidade',
-    type: 'text',
-  },
-  {
-    id: 'estado',
-    label: 'Estado',
-    type: 'text',
-  },
-];
+import '../src/style.css';
 
 const App = () => {
-  const [form, setForm] = React.useState(
-    formFields.reduce((acc, field) => {
-      return { ...acc, [field.id]: '' };
-    }, {}),
-  );
-
-  const [response, setResponse] = React.useState(null);
-
-  function handleChange({ target }) {
-    const { id, value } = target;
-    setForm({ ...form, [id]: value });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      // form é o objeto com os dados do formulário
-      body: JSON.stringify(form),
-    }).then((r) => {
-      setResponse(r);
-    });
-  }
+  const dados = [
+    {
+      nome: 'Period',
+      value: ['2016Q1', '2016Q2', '2016Q3', '2016Q4', '2017Q1'],
+      bgColor: ['#e0e0e0'],
+      borderTop: ['2px solid #a8a8a8'],
+      classe: 'teste',
+    },
+    {
+      nome: 'Android',
+      value: ['83.4%', '87.6%', '86.8%', '81.4%', '85.0%'],
+      bgColor: ['#e1f2c3'],
+      borderTop: ['2px solid #80a048'],
+      classe: '',
+    },
+    {
+      nome: 'iOS',
+      value: ['15.4%', '11.6%', '12.8%', '18.4%', '14.0%'],
+      bgColor: ['#c6d8f1'],
+      borderTop: ['2px solid #4068a0'],
+      classe: '',
+    },
+    {
+      nome: 'Windows Phone',
+      value: ['0.8%', '0.4%', '0.3%', '0.2%', '0.1%'],
+      bgColor: ['#dfd5ed'],
+      borderTop: ['2px solid #4068a0'],
+      classe: '',
+    },
+    {
+      nome: 'Others',
+      value: ['0.4%', '0.3%', '0.4%', '0.2%', '0.1%'],
+      bgColor: ['#f1dcc9'],
+      borderTop: ['2px solid #d88030'],
+      classe: '',
+    },
+  ];
 
   return (
-    <form onSubmit={handleSubmit}>
-      {formFields.map(({ label, id, type }) => (
-        <div key={id}>
-          <label htmlFor={id}>{label}</label>
-          <input type={type} id={id} value={form[id]} onChange={handleChange} />
+    <div className="grid">
+      {dados.map(({ nome, value, bgColor, borderTop, classe }, index) => (
+        <div key={index} style={{ textAlign: 'center' }}>
+          <h4
+            style={{
+              backgroundColor: bgColor,
+              borderTop: borderTop,
+              padding: '5px',
+            }}
+          >
+            {nome}
+          </h4>
+          {value.map((valor, index) => (
+            <p key={index} className={classe}>
+              {valor}
+            </p>
+          ))}
         </div>
       ))}
-      {response && response.ok && <p> Formulario enviado </p>}
-      <button>Enviar</button>
-    </form>
+    </div>
   );
 };
 
